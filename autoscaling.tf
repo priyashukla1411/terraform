84e65f72eff499a968664431938dcf9057ed3a0f
priyashukla1411 <100112297+priyashukla1411@users.noreply.github.com>
2022-05-31 18:45:59 +0530

    Update autoscaling.tf

--- autoscaling.tf
+++ autoscaling.tf
@@ -1,32 +1,4 @@
-### Creating EC2 instance
-
-# resource "aws_instance" "EC2" {
-#   ami           = "ami-0756a1c858554433e"
-#   instance_type = "t2.micro"
-#   key_name = "shukla"
-#    tags                         = {
-#         "Name" = "Terraform"
-#     }
-# }
-# ### Creating Security Group for EC2
-# resource "aws_security_group" "EC2" {
-#   name = "terraform-example-instance"
-#   ingress {
-#     from_port = 8080
-#     to_port = 8080
-#     protocol = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-#   ingress {
-#     from_port   = 22
-#     to_port     = 22
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
-
-
-# ## Creating Launch Configuration
+## Creating Launch Configuration
 resource "aws_launch_configuration" "example" {
   image_id               = "ami-0756a1c858554433e"
   instance_type          = "t2.micro"
@@ -44,7 +16,6 @@ resource "aws_launch_configuration" "example" {
   }
 }
 
-
 ## Creating AutoScaling Group
 resource "aws_autoscaling_group" "example" {
   launch_configuration = "${aws_launch_configuration.example.id}"
@@ -60,45 +31,6 @@ resource "aws_autoscaling_group" "example" {
   }
 }
 
-
-## Security Group for ELB
-# resource "aws_security_group" "elb" {
-#   name = "terraform-example-elb"
-#   egress {
-#     from_port = 0
-#     to_port = 0
-#     protocol = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-#   ingress {
-#     from_port = 80
-#     to_port = 80
-#     protocol = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
-
-
-# ### Creating ELB
-# resource "aws_elb" "example" {
-#   name = "terraform-asg-example"
-
-#   security_groups = ["${aws_security_group.elb.id}"]
-#   availability_zones = ["ap-south-1a"]
-#   health_check {
-#     healthy_threshold = 2
-#     unhealthy_threshold = 2
-#     timeout = 3
-#     interval = 30
-#     target = "HTTP:8080/"
-#   }
-#   listener {
-#     lb_port = 80
-#     lb_protocol = "http"
-#     instance_port = "8080"
-#     instance_protocol = "http"
-#   }
-# }
 resource "aws_autoscalingplans_scaling_plan" "example" {
   name = "autoooo-scalinggggg"
   application_source {
